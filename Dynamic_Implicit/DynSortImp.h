be9ff841f06e75bb16abe16496a40de19e276756
Илья Богданов <ibogdanov@bmstu.ru>
2021-02-23 18:00:16 +0000

    Замечания по DynSortImp.h

--- Dynamic_Implicit/DynSortImp.h
+++ Dynamic_Implicit/DynSortImp.h
@@ -1,3 +1,8 @@
 #pragma once
 
+//!!! Тут должна быть шапка, включающаю условные макросы для
+//!!! __declspec(dllexport) и __declspec(dllimport)
+//!!! Также нет extern "C".
+
+
 __declspec(dllexport) void SortDyn(int*, int);
